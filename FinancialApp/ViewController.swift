46ca8e67dc1d81a7de803d1577055fdd22c17212
Anderson Oliveira <cora@MacBook-Pro-de-Cora-2.local>
2022-09-08 09:56:43 -0300

    initial setup.

---
+++ FinancialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinancialApp
+//
+//  Created by Cora on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
